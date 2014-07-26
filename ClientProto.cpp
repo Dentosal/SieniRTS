68996bd4e9649c65060473159d3881f236f5f232
MusserO <iiro.kumpulainen@netikka.fi>
2014-07-26 14:15:47 +0300

    Ascii-sieni

--- ClientProto.cpp
+++ ClientProto.cpp
@@ -10,55 +10,78 @@ int main() {
   
     
   
-  sf::ContextSettings contextSettings;  
-    
-  contextSettings.depthBits = 32;
-  // window name
-  sf::RenderWindow window(sf::VideoMode(640, 480), "SieniRTS", sf::Style::Default, contextSettings);
-  window.setActive();
-  
-  // create clock
-  sf::Clock clock;
-  
-  // tehrään siäni
-  /*TODO: ascii siäni
-   
-   
-   */
-  sf::Texture sieni;
-  sieni.loadFromFile("Images/DefaultSieni.png");
-  
-  // main window loop
-  while (window.isOpen()) {
+    sf::ContextSettings contextSettings;  
 
-    // scan for events and react to them
-    sf::Event event;
-    while (window.pollEvent(event)) {
+    contextSettings.depthBits = 32;
+    // window name
+    sf::RenderWindow window(sf::VideoMode(640, 480), "SieniRTS", sf::Style::Default, contextSettings);
+    window.setActive();
 
-      // close the window
-//      if (event.type == sf::Event::Closed) {
-//	window.close();
-//      }(event.type == sf::Event::Closed)
-      if ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Escape)) {
-	window.close();
-      }
-      
-      // välilyönti tekee sienisiä asioita
-      if (((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Space)) || (event.type == sf::Event::Closed)) {
-	system("start chrome.exe http://sieni.es");
-      }
+    // create clock
+    sf::Clock clock;
 
-      // mouse should do stuff
-      if (event.type == sf::Event::MouseButtonPressed) { // is this the right event?
-          window.draw(sieni);
-      }
-	
+    // tehr��n si�ni
+    /*
+     *
+                 ___..._
+         _,--'       "`-.
+       ,'.  .            \
+     ,/:. .     .       .'
+     |;..  .      _..--'
+     `--:...-,-'""\
+             |:.  `.
+             l;.   l
+             `|:.   |
+              |:.   `.,
+             .l;.    j, ,
+          `. \`;:.   //,/
+           .\\)`;,|\'/(
+            ` `itz `(,
+     * 
+     */
 
+    sf::Texture texture1;
+    if (!texture1.loadFromFile("resources/DefaultSieni.png")) {    
+        // error...
     }
-    // draw everything on the sreen
-    window.display(); 
-  }
-  // close the program when the loop breaks
-  return EXIT_SUCCESS;
+    sf::Sprite sieni;
+    sieni.setScale(hahmoSize - sf::Vector2f(3, 3));
+    sieni.setTexture(texture1);
+    sieni.setColor(sf::Color(0, 255, 255));
+
+    // main window loop
+    while (window.isOpen()) {
+
+        // scan for events and react to them
+        sf::Event event;
+        while (window.pollEvent(event)) {
+
+            // close the window
+            // if (event.type == sf::Event::Closed) {
+            // window.close();
+            // }(event.type == sf::Event::Closed)
+            if ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Escape)) {
+                window.close();
+            }
+
+            // välilyönti tekee sienisiä asioita
+            if (((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Space)) || (event.type == sf::Event::Closed)) {
+                system("start chrome.exe http://sieni.es");
+            }
+
+            // mouse should do stuff
+            if (event.type == sf::Event::MouseButtonPressed) { // is this the right event?
+
+                sieni.setPosition(event.x,event.y);
+                window.draw(sieni);
+            }
+
+
+        }
+        // draw everything on the sreen
+        window.display(); 
+    }
+    // close the program when the loop breaks
+    return EXIT_SUCCESS;
 }
 
