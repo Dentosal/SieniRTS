1c227a80e619c91110e135b26b6e72b0159f7bef
Petzku <petzku.huuskonen@gmail.com>
2014-07-26 11:39:46 +0300

    ClientProto saatta joskus toimiakin!

--- ClientProto.cpp
+++ ClientProto.cpp
@@ -1,12 +1,26 @@
 #include <SFML/Window.hpp>
 
-int main()
-{
+int main() {
   
   sf::Window window(sf::VideoMode(640, 480), "SieniRTS", sf::Style::Default, contextSettings);
   window.setActive();
   
+  sf::Clock clock;
+  
   while (window.isOpen) {
+    sf:Event event;
+    
+    while (window.pollEvent(event)) {
+
+      if (event.type == sf::Event::Closed) {
+	window.close();
+      }
+
+      if ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Escape)) {
+	window.close();
+      }
+
+    }
     
     window.update; 
   }
