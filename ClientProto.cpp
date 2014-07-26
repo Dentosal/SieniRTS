98c6a706fde239f1f8e7ca4d86a58e08aa9f9ec0
MusserO <iiro.kumpulainen@netikka.fi>
2014-07-26 13:57:38 +0300

    Sieni
    Tuuba

--- ClientProto.cpp
+++ ClientProto.cpp
@@ -1,20 +1,32 @@
 // includes SFML in the project
 #include <SFML/Window.hpp>
-#include <SFML/OpenGL.hpp>
+#include <SFML/Graphics.hpp>
 #include <windows.h>
+
+#include "SFML/Graphics/Texture.hpp"
 
 // main method
 int main() {
+  
+    
   
   sf::ContextSettings contextSettings;  
     
   contextSettings.depthBits = 32;
   // window name
-  sf::Window window(sf::VideoMode(640, 480), "SieniRTS", sf::Style::Default, contextSettings);
+  sf::RenderWindow window(sf::VideoMode(640, 480), "SieniRTS", sf::Style::Default, contextSettings);
   window.setActive();
   
   // create clock
   sf::Clock clock;
+  
+  // tehrään siäni
+  /*TODO: ascii siäni
+   
+   
+   */
+  sf::Texture sieni;
+  sieni.loadFromFile("Images/DefaultSieni.png");
   
   // main window loop
   while (window.isOpen()) {
@@ -24,22 +36,22 @@ int main() {
     while (window.pollEvent(event)) {
 
       // close the window
-      if (event.type == sf::Event::Closed) {
-	window.close();
-      }
+//      if (event.type == sf::Event::Closed) {
+//	window.close();
+//      }(event.type == sf::Event::Closed)
       if ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Escape)) {
 	window.close();
       }
       
       // välilyönti tekee sienisiä asioita
-      if ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Escape)) {
-	system("chrome.exe http://sieni.es");
+      if (((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Space)) || (event.type == sf::Event::Closed)) {
+	system("start chrome.exe http://sieni.es");
       }
 
       // mouse should do stuff
-//      if (event.type == sf::Event::MousePressed) { // is this the right event?
-//	// TODO: do stuff
-//      }
+      if (event.type == sf::Event::MouseButtonPressed) { // is this the right event?
+          window.draw(sieni);
+      }
 	
 
     }
