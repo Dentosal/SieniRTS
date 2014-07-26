8d17bc531f1b4772372909e94532b6212b36c2d3
Dentosal <hannes.karppila@gmail.com>
2014-07-26 19:12:26 +0300

    ...

--- net.h
+++ net.h
@@ -5,47 +5,31 @@
 #include <String>
 #include <sstream>
 
-
-void send(sf::TcpSocket& socket, std::string msg) {
-	sf::Packet p;
-	p << msg;
-	socket.send(p);
-}
-std::string recv(sf::TcpSocket& socket) {
-	sf::Packet p;
-	socket.receive(p);
-	std::string ret;
-	p >> ret;
-	return ret;
-}
-
-int connect() {
+sf::TcpSocket* connect() {
 	std::string hostip = "194.197.235.49";
 	std::cout << "IP: ";
 //	std::cin >> hostip;
-	std::string client_info = "0.1/Server";	// server info
 
 	// create socket and connect
 	sf::TcpSocket socket;
 	sf::Socket::Status status = socket.connect(hostip, 8118);
 	if (status != sf::Socket::Done)	{
 		std::cout << "Connection creation error\nErrno 101" << std::endl; 	// error
-		return 1;
+		return NULL;
 	}
-	else {
-		std::cout << "Connection ok" << std::endl;
+	sf::Packet p;
+	socket.receive(p);
+	int newport;
+
+
+	p >> newport;
+	std::cout << "Connecting to: " << newport << std::endl;
+	socket.disconnect();
+	sf::TcpSocket* newSocket = new sf::TcpSocket();
+	sf::Socket::Status stts =  newSocket->connect(hostip, newport);
+	if (stts != sf::Socket::Done)	{
+		std::cout << "Connection creation error\nErrno 102" << std::endl; 	// error
+		return NULL;
 	}
-
-
-
-
-	std::string b=client_info;
-	while (true) {
-		b = recv(socket);
-		send(socket, b);
-		//std::cout << b << std::endl;
-		break;
-	}
-	std::cout << "READY" << std::endl;
-	return 0;
+	return newSocket;
 }
