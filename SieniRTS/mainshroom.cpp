12774795ca919f8d54b53a416178a5d6a17abb4c
LarePvl <laurivir@gmail.com>
2014-07-26 12:59:21 +0300

    Ängetään tänne SFML sekä esimerkki-Netbeans-projekti niin on jotain toivoa että sama koodi saadaan käännettyä muillakin windows-koneilla?

---
+++ SieniRTS/mainshroom.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   mainshroom.cpp
+ * Author: Lauri
+ *
+ * Created on 26. heinäkuuta 2014, 10:19
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
