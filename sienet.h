9de427e168cf19085c223403c82d2b35add4d5a4
Dentosal <hannes.karppila@gmail.com>
2014-07-27 13:04:51 +0300

    Spaces->Tabs

--- sienet.h
+++ sienet.h
@@ -5,197 +5,197 @@
 using namespace std;
 
 class Sieni {
-    int X, Y, targetX, targetY, Health, State, Team, Type;
+	int X, Y, targetX, targetY, Health, State, Team, Type;
   public:
-    void set_values (int,int,int,int,int,int,int,int);
-    int area () {return X;}
-    
-    void setPos(int,int);
-    int getX();
-    int getY();
-    
-    void setTarget(int,int);
-    int getTargetX();
-    int getTargetY();
-    
-    void setHealth();
-    void heal(int);
-    int getHealth();
-    
-    void setState(int);
-    int getState();
-    
-    void setTeam(int);
-    int getTeam();
-    
-    void setType(int);
-    int getType();
+	void set_values (int,int,int,int,int,int,int,int);
+	int area () {return X;}
+	
+	void setPos(int,int);
+	int getX();
+	int getY();
+	
+	void setTarget(int,int);
+	int getTargetX();
+	int getTargetY();
+	
+	void setHealth();
+	void heal(int);
+	int getHealth();
+	
+	void setState(int);
+	int getState();
+	
+	void setTeam(int);
+	int getTeam();
+	
+	void setType(int);
+	int getType();
 };
 
 
 
 void Sieni::setPos(int x, int y)
-    {
-    X = x;
-    Y = y;
-    }
+	{
+	X = x;
+	Y = y;
+	}
 int Sieni::getX()
-    {
-    return X;
-    }
+	{
+	return X;
+	}
 int Sieni::getY()
-    {
-    return Y;
-    }
+	{
+	return Y;
+	}
 void Sieni::setTarget(int tx, int ty)
-    {
-    targetX = tx;
-    targetY = ty;
-    }
+	{
+	targetX = tx;
+	targetY = ty;
+	}
 int Sieni::getTargetX()
-    {
-    return targetX;
-    }
+	{
+	return targetX;
+	}
 int Sieni::getTargetY()
-    {
-    return Y;
-    }
+	{
+	return Y;
+	}
 void Sieni::setHealth(int hp) {
-    Health = hp;
+	Health = hp;
 }
 void Sieni::heal(int hp)
-    {
-    Health += hp;
-    }
+	{
+	Health += hp;
+	}
 int Sieni::getHealth()
-    {
-    return Health;
-    }
+	{
+	return Health;
+	}
 void Sieni::setState(int state)
-    {
-    State = state;
-    }
+	{
+	State = state;
+	}
 int Sieni::getState()
-    {
-    return State;
-    }
+	{
+	return State;
+	}
 void Sieni::setTeam(int team)
-    {
-    Team = team;
-    }
+	{
+	Team = team;
+	}
 int Sieni::getTeam()
-    {
-    return Team;
-    }
+	{
+	return Team;
+	}
 void Sieni::setType(int type)
-    {
-    Type = type;
-    }
+	{
+	Type = type;
+	}
 int Sieni::getType()
-    {
-    return Type;
-    }
+	{
+	return Type;
+	}
 
 class House
-    {
-    int X, Y, targetX, targetY, sizeX, sizeY, Health, State, Team, Type;
+	{
+	int X, Y, targetX, targetY, sizeX, sizeY, Health, State, Team, Type;
 public:
-    void setPos(int,int);
-    int getX();
-    int getY();
-    
-    void setTarget(int,int);
-    int getTargetX();
-    int getTargetY();
-    
-    void setSize(int,int);
-    int getSizeX();
-    int getSizeY();
-    
-    void heal(int);
-    int getHealth();
-    
-    void setState(int);
-    int getState();
-    
-    void setTeam(int);
-    int getTeam();
-    
-    void setType(int);
-    int getType();
-    };
+	void setPos(int,int);
+	int getX();
+	int getY();
+	
+	void setTarget(int,int);
+	int getTargetX();
+	int getTargetY();
+	
+	void setSize(int,int);
+	int getSizeX();
+	int getSizeY();
+	
+	void heal(int);
+	int getHealth();
+	
+	void setState(int);
+	int getState();
+	
+	void setTeam(int);
+	int getTeam();
+	
+	void setType(int);
+	int getType();
+	};
 
 void House::setPos(int x, int y)
-    {
-    X = x;
-    Y = y;
-    }
+	{
+	X = x;
+	Y = y;
+	}
 int House::getX()
-    {
-    return X;
-    }
+	{
+	return X;
+	}
 int House::getY()
-    {
-    return Y;
-    }
+	{
+	return Y;
+	}
 void House::setTarget(int tx, int ty)
-    {
-    targetX = tx;
-    targetY = ty;
-    }
+	{
+	targetX = tx;
+	targetY = ty;
+	}
 int House::getTargetX()
-    {
-    return targetX;
-    }
+	{
+	return targetX;
+	}
 int House::getTargetY()
-    {
-    return Y;
-    }
+	{
+	return Y;
+	}
 void House::setPos(int sX, int sY)
-    {
-    sizeX = sX;
-    sizeY = sX;
-    }
+	{
+	sizeX = sX;
+	sizeY = sX;
+	}
 int House::getSizeX()
-    {
-    return sizeX;
-    }
+	{
+	return sizeX;
+	}
 int House::getSizeY()
-    {
-    return sizeY;
-    }
+	{
+	return sizeY;
+	}
 void House::heal(int hp)
-    {
-    Health += hp;
-    }
+	{
+	Health += hp;
+	}
 int House::getHealth()
-    {
-    return Health;
-    }
+	{
+	return Health;
+	}
 void House::setState(int state)
-    {
-    State = state;
-    }
+	{
+	State = state;
+	}
 int House::getState()
-    {
-    return State;
-    }
+	{
+	return State;
+	}
 void House::setTeam(int team)
-    {
-    Team = team;
-    }
+	{
+	Team = team;
+	}
 int House::getTeam()
-    {
-    return Team;
-    }
+	{
+	return Team;
+	}
 void House::setType(int type)
-    {
-    Type = type;
-    }
+	{
+	Type = type;
+	}
 int House::getType()
-    {
-    return Type;
-    }
+	{
+	return Type;
+	}
 
 
 /*
